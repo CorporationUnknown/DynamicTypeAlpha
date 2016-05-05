522ffcdec25cb05ee695a51fe49b928388f2863c
Paul Goracke <pgoracke@onemedical.com>
2016-05-05 16:37:03 -0700

    New app

---
+++ DynamicTypeAlpha/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DynamicTypeAlpha
+//
+//  Created by Paul Goracke on 5/5/16.
+//  Copyright © 2016 Corporation Unknown. All rights reserved.
+//
+
+@import UIKit;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DynamicTypeAlpha/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DynamicTypeAlpha
+//
+//  Created by Paul Goracke on 5/5/16.
+//  Copyright © 2016 Corporation Unknown. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
