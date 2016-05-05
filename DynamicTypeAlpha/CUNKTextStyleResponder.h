fae354ac8e834c796a2b35681c2bf00d5c28e7b4
Paul Goracke <pgoracke@onemedical.com>
2016-05-05 16:44:40 -0700

    Initial CUNKTextStyleResponder

---
+++ DynamicTypeAlpha/CUNKTextStyleResponder.h
@@ -0,0 +1,13 @@
+//
+//  CUNKTextStyleResponder.h
+//  DynamicTypeDemo
+//
+//  Created by Paul Goracke on 5/5/16.
+//  Copyright © 2016 Corporation Unknown. All rights reserved.
+//
+
+@import UIKit;
+
+@interface CUNKTextStyleResponder : UIResponder
+
+@end
---
+++ DynamicTypeAlpha/CUNKTextStyleResponder.m
@@ -0,0 +1,17 @@
+//
+//  CUNKTextStyleResponder.m
+//  DynamicTypeDemo
+//
+//  Created by Paul Goracke on 5/5/16.
+//  Copyright © 2016 Corporation Unknown. All rights reserved.
+//
+
+#import "CUNKTextStyleResponder.h"
+
+@implementation CUNKTextStyleResponder
+
+- (void) setFontForLabel:(UILabel*)label {
+    
+}
+
+@end
