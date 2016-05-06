4e18d87741c4a37a55e93fb041fad9715e795e2e
Paul Goracke <pgoracke@onemedical.com>
2016-05-05 17:45:42 -0700

    Add labels

--- DynamicTypeAlpha/ViewController.m
+++ DynamicTypeAlpha/ViewController.m
@@ -10,6 +10,9 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *headlineLabel;
+@property (weak, nonatomic) IBOutlet UILabel *subheadLabel;
+
 @end
 
 @implementation ViewController
